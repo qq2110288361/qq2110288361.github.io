8316f8145e9a3d6e75da9ce1093e23d01a36e4f5
qq2110288361 <qq2110288361@hotmail.com>
2024-10-14 19:20:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '友链监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2704459-4b050e7d3c425f6e90b95047',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'Luogu',
+      url: 'https://www.luogu.com.cn/user/1123573'
+    },
+    {
+      text: 'Blog',
+      url: 'https://xyx404.github.io/'
+    },
+  ],
+};
